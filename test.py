5272deed8581d4b8bf5771eef0854d22edbff875
davidoodugama <davidoodugama1999@gmail.com>
2022-05-08 01:54:28 +0530

    Extracting Topics

--- test.py
+++ test.py
@@ -1,45 +1,52 @@
-import cv2
-import numpy as np
-import easyocr
+# import cv2
+# import numpy as np
+# import easyocr
 
-def nothing(x):
-    pass
+# def nothing(x):
+#     pass
 
-#cap = cv2.VideoCapture(0)
-# cv2.namedWindow("Trackbars")
+# #cap = cv2.VideoCapture(0)
+# # cv2.namedWindow("Trackbars")
 
-# cv2.createTrackbar("L – H", "Trackbars", 0, 179, nothing)
-# cv2.createTrackbar("L – S", "Trackbars", 0, 255, nothing)
-# cv2.createTrackbar("L – V", "Trackbars", 0, 255, nothing)
-# cv2.createTrackbar("U – H", "Trackbars", 179, 179, nothing)
-# cv2.createTrackbar("U – S", "Trackbars", 255, 255, nothing)
-# cv2.createTrackbar("U – V", "Trackbars", 255, 255, nothing)
+# # cv2.createTrackbar("L – H", "Trackbars", 0, 179, nothing)
+# # cv2.createTrackbar("L – S", "Trackbars", 0, 255, nothing)
+# # cv2.createTrackbar("L – V", "Trackbars", 0, 255, nothing)
+# # cv2.createTrackbar("U – H", "Trackbars", 179, 179, nothing)
+# # cv2.createTrackbar("U – S", "Trackbars", 255, 255, nothing)
+# # cv2.createTrackbar("U – V", "Trackbars", 255, 255, nothing)
 
-while True:
-    #_, frame = cap.read()
-    img = cv2.imread("image.jpg")
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+# while True:
+#     #_, frame = cap.read()
+#     img = cv2.imread("image.jpg")
+#     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-#     l_h = cv2.getTrackbarPos("L – H", "Trackbars")
-#     l_s = cv2.getTrackbarPos("L – S", "Trackbars")
-#     l_v = cv2.getTrackbarPos("L – V", "Trackbars")
-#     u_h = cv2.getTrackbarPos("U – H", "Trackbars")
-#     u_s = cv2.getTrackbarPos("U – S", "Trackbars")
-#     u_v = cv2.getTrackbarPos("U – V", "Trackbars")
+# #     l_h = cv2.getTrackbarPos("L – H", "Trackbars")
+# #     l_s = cv2.getTrackbarPos("L – S", "Trackbars")
+# #     l_v = cv2.getTrackbarPos("L – V", "Trackbars")
+# #     u_h = cv2.getTrackbarPos("U – H", "Trackbars")
+# #     u_s = cv2.getTrackbarPos("U – S", "Trackbars")
+# #     u_v = cv2.getTrackbarPos("U – V", "Trackbars")
 
-    lower_blue = np.array([0, 0, 0])
-    upper_blue = np.array([179,255,204])
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+#     lower_blue = np.array([0, 0, 0])
+#     upper_blue = np.array([179,255,204])
+#     mask = cv2.inRange(hsv, lower_blue, upper_blue)
     
     
 
-#     result = cv2.bitwise_and(img, img, mask=mask)
+# #     result = cv2.bitwise_and(img, img, mask=mask)
 
-#     cv2.imshow("img", img)
-    cv2.imshow("mask", mask)
+# #     cv2.imshow("img", img)
+#     cv2.imshow("mask", mask)
 
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
+#     key = cv2.waitKey(1)
+#     if key == 27:
+#         break
 
-cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
+import sp_recog as sr
+r = sr.Recognizer()
+harvard = sr.AudioFile('test.wav')
+with harvard as source:
+    audio = r.record(source)
+
+r.recognize_google(audio)
